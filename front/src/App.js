a7dfb3209788e5676996039606f07081481ae590
AlexGrimpeur <moreaualexandre2002@gmail.com>
2023-05-15 23:10:24 +0200

    mise en place d'une premiere structure coté front

---
+++ front/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HAAAAAAAAAA</h1>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='home'>
+      text test de la page home
+    </div>
+  )
+}
