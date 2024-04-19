8a25027eaf81ae6031be3b8d8cd92e353b121702
Alexandre <moreaualexandre2002@gmail.com>
2024-04-19 17:24:23 +0200

    refactor(utils): move generateCode function for better modularity
    Move the generateVerificationCode function, which creates a 6-digit code
    for email verification and tracks its expiration, to utils.js. This
    change improves code modularity and separation of concerns, making the
    codebase easier to manage and update.

--- back/src/utils/expiration.utils.js
+++ back/src/utils/expiration.utils.js
@@ -1,2 +1,24 @@
-// Génération de code à 6 chiffres
-// Génération de date d'expiration
+
+/**
+ * Génère un code de vérification à 6 chiffres.
+ */
+
+const generateVerificationCode = () => {
+    return Math.floor(100000 + Math.random() * 900000).toString();
+};
+
+/**
+ * Génère une date d'expiration pour le code de vérification.
+ * @param {number} durationInMinutes - Durée en minutes avant que le code expire.
+ */
+
+const generateExpirationDate = (durationInMinutes = 15) => {
+    const offsetInHours = 2;  // Décalage fixe de +2 heures
+    const now = new Date();
+    return new Date(now.getTime() + durationInMinutes * 60000 + offsetInHours *3600000);
+};
+
+module.exports = {
+    generateVerificationCode,
+    generateExpirationDate
+};
