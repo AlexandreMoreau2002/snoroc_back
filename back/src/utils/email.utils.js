69c89964aab068987890b020f45e2030e9776e0b
Alexandre <moreaualexandre2002@gmail.com>
2024-04-19 22:49:17 +0200

    feat(email.utils): create new utility file for email operations
    Add a new utility file, email.utils, to handle all email-related
    operations such as sending notifications and verifying user emails. This
    centralization simplifies our codebase and improves maintainability by
    segregating email operations from other parts of our backend logic.

--- back/src/utils/email.utils.js
+++ back/src/utils/email.utils.js
@@ -1,30 +1,46 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-async function sendEmail({from, to, subject, text, html}) {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    });
+/**
+ * Asynchronously sends an email using predefined SMTP settings.
+ *
+ * @param {Object} mailData - Object containing email parameters.
+ * @param {string} mailData.from - The email address of the sender.
+ * @param {string} mailData.to - The email address(es) of the recipient(s).
+ * @param {string} mailData.subject - The subject line of the email.
+ * @param {string} mailData.text - The plain text body of the email.
+ * @param {string} mailData.html - The HTML body of the email (optional).
+ * @returns {Promise<Object>} - A promise that resolves to an object indicating the result of the email sending operation.
+ */
 
-    const mailOptions = {
-        from,   // Qui envoie l'email
-        to,     // À qui
-        subject,  // Sujet de l'email
-        text,   // Corps de l'email en texte simple
-        html    // Corps de l'email en HTML (optionnel)
-    };
+async function sendEmail({ from, to, subject, text, html }) {
+  // Create a transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    service: "gmail", // SMTP service provider
+    auth: {
+      user: process.env.EMAIL_USERNAME, // SMTP username from environment variables
+      pass: process.env.EMAIL_PASSWORD, // SMTP password from environment variables
+    },
+  });
 
-    try {
-        const info = await transporter.sendMail(mailOptions);
-        console.log('Email sent: ' + info.response);
-        return { success: true, message: 'Email sent', info: info };
-    } catch (error) {
-        console.error('Error sending email: ', error);
-        return { success: false, message: 'Failed to send email', error: error };
-    }
+  // Setup email data (envelope)
+  const mailOptions = {
+    from,
+    to,
+    subject,
+    text,
+    html,
+  };
+
+  try {
+    // Attempt to send the email and log the response
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: " + info.response);
+    return { success: true, message: "Email sent", info: info };
+  } catch (error) {
+    // Log any errors that occur
+    console.error("Error sending email: ", error);
+    return { success: false, message: "Failed to send email", error: error };
+  }
 }
 
 module.exports = { sendEmail };
