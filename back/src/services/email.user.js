2d7ec21acd04c27ee8f256e11fa624c3e64ee33a
Alexandre <moreaualexandre2002@gmail.com>
2024-04-19 22:53:21 +0200

    feat(email.user): create file for handling user email operations
    Introduce a new file, email.user, dedicated to handling email operations
    for users. Currently, it includes functionality for sending email
    verification messages. This separation enhances code organization and
    allows for easy expansion as we add more types of user emails in the
    future.

---
+++ back/src/services/email.user.js
@@ -0,0 +1,21 @@
+/**
+ * Prépare les données de l'email pour l'envoi du code de vérification.
+ * @param {string} to - Adresse email du destinataire.
+ * @param {string} code - Code de vérification à envoyer.
+ * @returns {Object} Les données de l'email.
+ */
+
+const emailDataVerification = (to, code) => {
+    return {
+        from: process.env.EMAIL_USERNAME,
+        to: to,
+        subject: "Activation de votre compte",
+        text: `Votre code d'activation est : ${code}. Il est valide pour 15 minutes.`,
+        html: `<p>Votre code d'activation est : </p><br><p><strong>${code}</strong></p><br> <p>Il est valide pour <strong>15 minutes</strong>.</p>`
+    };
+};
+
+module.exports = { emailDataVerification };
+
+
+// other email template function for user
