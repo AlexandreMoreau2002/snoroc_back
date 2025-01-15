f296a6eb40c2abfc6b2982eb165671a0397af8a5
Alexandre <moreaualexandre2002@gmail.com>
2025-01-15 22:38:28 +0100

    feat(Login): creat login component

--- front/src/pages/visitor/Profile.jsx
+++ front/src/pages/visitor/Profile.jsx
@@ -1,6 +1,7 @@
 // src/pages/visitor/User/UserProfile.jsx
 import React, { useEffect } from 'react'
 import getUser from '../../services/user/getUser'
+import { Login } from '../../components/login'
 
 export default function UserProfile() {
   useEffect(() => {
@@ -24,6 +25,10 @@ export default function UserProfile() {
     <div>
       <h1>Profil utilisateur</h1>
       <p>Regarde la console pour les données utilisateur.</p>
+      <div>
+        <h2>Composant login</h2>
+        <Login />
+      </div>
     </div>
   )
 }
